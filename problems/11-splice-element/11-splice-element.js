a92131d676373e1c0d043fc3f150a3e40d4f5d71
Elias Friedman <efriedman02@gmail.com>
2019-05-07 18:50:50 -0400

    finished splice element problem

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,20 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+  for(let i = index; i < someArr.length; i++) {
+    someArr[i] = someArr[i + 1]
+  }
+  index < someArr.length && someArr.pop()
 }
+
+let testArrays = [
+  {ary: [0, 1, 2, 3, 4, 5], toRemove: 2},
+  {ary: [0], toRemove: 3},
+  {ary: [0, 1, 2, 3], toRemove: 3}
+]
+
+testArrays.forEach(array => {
+  console.log('before', array.ary)
+  spliceElement(array.ary, array.toRemove)
+  console.log('after', array.ary)
+})
