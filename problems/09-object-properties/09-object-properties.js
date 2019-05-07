1de839e300c3ee1f9f429b880f27d08bd425edc7
Elias Friedman <efriedman02@gmail.com>
2019-05-07 18:43:35 -0400

    finished object assignments

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,16 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+  for(let key in someObj) {
+    console.log(key, '  ', someObj[key])
+  }
 }
+
+let objectsToTest = [
+  module,
+  process
+]
+
+objectsToTest.forEach(obj => {
+  objectProperties(obj)
+})
--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,30 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+const people = []
+
+people.push({
+  name: 'Xohn Mold',
+  age: NaN
+})
+
+function makePerson(name, age) {
+  this.name = name
+  this.age = age
+}
+
+people.push(new makePerson('Captain Swab', 31))
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}
+
+people.push(new Person(undefined, Infinity))
+
+people.forEach(person => {
+  console.log(`name: ${person.name}   age: ${person.age}`)
+})
