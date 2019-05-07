9b77ffd8c0d85f9fefd31718054530699a40ad74
Elias Friedman <efriedman02@gmail.com>
2019-05-07 16:30:58 -0400

    completed factorial calculator

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -1,5 +1,10 @@
 /* 4. Factorial
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
-const factorial = sumNum => sumNum ? sumNum * factorial(sumNum) : 1
- 
+const factorial = sumNum => sumNum ? sumNum * factorial(sumNum - 1) : 1
+
+let numbs = [5, 12, 2, 0, 13]
+
+numbs.forEach(numb => {
+  console.log(`${numb}! = ${factorial(numb)}`)
+})
