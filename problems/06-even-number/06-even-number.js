f121ca9279da445f1fc6bcca341bf7f7e48359c6
Elias Friedman <efriedman02@gmail.com>
2019-05-07 16:51:49 -0400

    completed even number problem

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,12 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
-function isEven(someNum) {
-  
-}
+const isEven = someNum => someNum / 2 === Math.floor(someNum / 2)
+
+let numbsToTest = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+]
+
+numbsToTest.forEach(numb => {
+  console.log(`the number ${numb} is even: ${isEven(numb)}`)
+})
