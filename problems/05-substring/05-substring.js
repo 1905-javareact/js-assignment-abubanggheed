772efaa25cfa70afde195c0e48ccd7096a258de5
Elias Friedman <efriedman02@gmail.com>
2019-05-07 16:47:01 -0400

    completed substring assignment

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,39 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+  if (typeof (someStr) !== 'string') {
+    throw 'you must input a string'
+  }
+  let returnString = ''
+  for (let i = startIndex; i < endIndex && i < someStr.length; i++) {
+    returnString += someStr[i]
+  }
+  return returnString
 }
+
+let toTry = [
+  {
+    str: 'here is a large string',
+    start: 3,
+    end: 15
+  },
+  {
+    str: '',
+    start: 0,
+    end: 4
+  },
+  {
+    str: () => {},
+    start: 1,
+    end: 5
+  }
+]
+
+toTry.forEach(input => {
+  try {
+    let output = substring(input.str, input.start, input.end)
+    console.log('for an input of', input, `we get ${output}`)
+  } catch (error) {
+    console.log(`for an input of`, input, `we got an error of "${error}"`)
+  }
+})
