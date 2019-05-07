75d8748cc3b8bc132f1a2b8a925b95ccef09f4a4
Elias Friedman <efriedman02@gmail.com>
2019-05-07 16:01:06 -0400

    wrote fib solution

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,18 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    let a = 0
+    let b = 0
+    let ret = 0
+    for(let i = 0; i < n; i++) {
+        ret = b + a || 1
+        a = b
+        b = ret
+    }
+    return ret
+    // this is much faster than the recursive solution
+}
 
+for(let i = 0; i < 11; i++) {
+    console.log(`fibonacci at ${i}: ${fib(i)}`)
 }
