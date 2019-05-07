629061ca97e37c1a443174b1152c919091273dbe
Elias Friedman <efriedman02@gmail.com>
2019-05-07 17:00:50 -0400

    finished palindrome problem

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,22 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
-}
+
+// I did 2 other palendrome solutions and have them here:
+// https://github.com/abubanggheed/select_js_challenges/blob/master/onelinepalindrome.js
+
+const reverseString = require('../03-reverse-string/03-reverse-string');
+
+const isPalindrome = someStr => someStr === reverseString(someStr)
+const isPalindrome2 = someStr => isPalindrome(someStr.replace(/ /g, '').toLowerCase())
+
+let stringsToTest = [
+  'helleh', '', 'iu34', 'graphle lhparg', 'Gag'
+]
+
+stringsToTest.forEach(string => {
+  console.log(`the string ${string},
+  is a rigid palendrom: ${isPalindrome(string)}.
+  It is a palendrome ignoring capitols and spaces: ${isPalindrome2(string)}
+  `)
+})
